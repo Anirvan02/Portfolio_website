55e532bd9e15d344c4051da1c91dd8aa0e87b67f
Anirvan Pandey <130135426+Anirvan02@users.noreply.github.com>
2024-08-17 22:37:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let navbar = document.querySelector('.header .navbar')
-
-document.querySelector('#menu').onclick = () =>{
-  navbar.classList.add('active');
-}
-
-document.querySelector('#close').onclick = () =>{
-  navbar.classList.remove('active');
-}
